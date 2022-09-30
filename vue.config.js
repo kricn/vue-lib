0dce8d67ff387d725a58e7cea7d928f85f6a3d5d
kricn <mnyt2436@126.com>
2022-10-01 00:56:50 +0800

    项目初始化

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,60 @@
-module.exports = {
-  transpileDependencies: true
+const NODE_ENV = 'development' // 开发环境 打包doc
+const path = require("path");
+// const components = require('./build/compontents.json');
+
+const baseConfig = {
+  chainWebpack: config => {
+    config.module
+      .rule('js')
+      .include
+      .add('/packages')
+      .end()
+      .use('babel')
+      .loader('babel-loader')
+      .tap(options => {
+        // 修改它的选项...
+        return options
+      })
+    config.resolve
+    .alias.set("@/", path.join(__dirname, "./examples"))
+      
+  },
+  // css: {
+  //   extract: false,
+  //   loaderOptions: {
+  //     // 给 sass-loader 传递选项
+  //     sass: {
+  //       data: `@import "public/css/ui.scss";`
+  //     }
+  //   },
+  // }
 }
+
+const devConfig = {
+  pages: {
+    index: {
+      entry: 'examples/main.ts',
+      template: 'public/index.html',
+      filename: 'index.html',
+    }
+  },
+
+  publicPath: './',
+  outputDir: 'docs',
+  ...baseConfig
+}
+
+const buildConfig = {
+  // configureWebpack: {
+  //   entry: components,
+  //   output: {
+  //     filename: '[name].js',
+  //     libraryTarget: 'commonjs2',
+  //   },
+  // },
+  // outputDir: 'lib',
+  // productionSourceMap: false,
+  // ...baseConfig
+}
+
+module.exports = NODE_ENV === 'development' ? devConfig : buildConfig;
