970ac0f4a656f02f973987543b6150f12bcf8d31
kricn <mnyt2436@126.com>
2022-10-02 17:56:34 +0800

    组件库文件夹

---
+++ packages/KButton/src/KButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="k-button">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+<script>
+export default {
+  name: "KButton"
+}
+</script>
+
+<style lang="scss">
+</style>
