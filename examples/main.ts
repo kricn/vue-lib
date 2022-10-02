e097627143aacfa7acc7559d2db3731db6cca382
kricn <mnyt2436@126.com>
2022-10-02 17:55:10 +0800

    将组件库导入例子中

--- examples/main.ts
+++ examples/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
-createApp(App).use(router).mount('#app')
+import OrangeUI from '@packages/index.js'
+import "@packages/theme-chalk/src/index.scss"
+const app = createApp(App)
+app.use(OrangeUI).use(router).mount('#app')
