9e7503f3b25cf526392a61e9b81a92ebb87607f7
陈凯旋 <chenkaixuan@doctortech.com.cn>
2022-10-11 14:29:29 +0800

    更新 package 下的组件

--- packages/index.js
+++ packages/index.js
@@ -1,7 +1,8 @@
 import Button from './button/index'
-
+import Icon from './icon/index'
 const components = [
-  Button
+  Button,
+  Icon
 ]
 
 // 定义 install 方法，接收 Vue 作为参数。如果使用 use 注册插件，则所有的组件都将被注册
